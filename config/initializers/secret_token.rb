e5f815d555f546daad8b80e8db911b46da579d2e
bojovs <bojovs@gmail.com>
2011-02-10 22:04:35 +0900

    added a sending csrf-token process for rails 3.0.4

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Panua::Application.config.secret_token = '168788faaeb772f6b9bd752107a313b287febd44c0c732521f989ae6fc6ebedc56739aca1830f44381ab1446965f09144fd37489248d51e40c74d1e92a114977'
+Panua::Application.config.secret_token = '9fe0864ea1c4e97f31d4c2353e969ca999e1a11f5022e201bbd3973299c2520189b1e9fc8a63911520d529c009445e533208980b09c857eb8df70721bd316282'
