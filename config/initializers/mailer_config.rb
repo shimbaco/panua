88830d7ff63d6d3531e23d5fae4da3478cc9b3d2
bojovs <bojovs@gmail.com>
2011-02-24 00:36:02 +0900

    updated

--- config/initializers/mailer_config.rb
+++ config/initializers/mailer_config.rb
@@ -8,13 +8,13 @@ if Rails.env.development?
     :authentication       => ApplicationSettings.smtp_authentication,
     :enable_starttls_auto => ApplicationSettings.smtp_enable_starttls_auto
   }
-elsif Rails.env.production?
-  ActionMailer::Base.smtp_settings = {
-    :address        => "smtp.sendgrid.net",
-    :port           => "25",
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => ENV['SENDGRID_DOMAIN']
-  }
+#elsif Rails.env.production?
+#  ActionMailer::Base.smtp_settings = {
+#    :address        => "smtp.sendgrid.net",
+#    :port           => "25",
+#    :authentication => :plain,
+#    :user_name      => ENV['SENDGRID_USERNAME'],
+#    :password       => ENV['SENDGRID_PASSWORD'],
+#    :domain         => ENV['SENDGRID_DOMAIN']
+#  }
 end
