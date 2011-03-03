27ab838368c247a4a6d99311fabd6e2f828fbe64
bojovs <bojovs@gmail.com>
2011-03-03 12:41:11 +0900

    update created_at format

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 module ApplicationHelper
 
   def page_title(text = nil)
@@ -6,5 +8,17 @@ module ApplicationHelper
       title += "#{text} | "
     end
     title += 'Panua'
+  end
+
+  def little_time_ago_in_words(from_time, include_seconds = false)
+    from_time = from_time.to_time
+    to_time = Time.now.to_time
+    distance_in_minutes = (((to_time - from_time).abs)/60).round
+
+    if distance_in_minutes < 1440
+      time_ago_in_words(from_time, include_seconds)
+    else
+      l(from_time)
+    end
   end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 require 'spec_helper'
 
 describe ApplicationHelper do
@@ -9,6 +11,18 @@ describe ApplicationHelper do
 
     it 'returns a combined title' do
       helper.page_title('welcome!').should == 'welcome! | Panua'
+    end
+  end
+
+  describe 'little_time_ago_in_words' do
+    it 'returns words because the datetime is within 24 hours' do
+      datetime = Time.now - 5.hours
+      helper.little_time_ago_in_words(datetime).should == '約5時間'
+    end
+
+    it 'returns datetime string because the datetime is without 24 hours' do
+      datetime = Time.now - 24.hours
+      helper.little_time_ago_in_words(datetime).should == datetime.strftime('%Y/%m/%d %H:%M:%S')
     end
   end
 end
